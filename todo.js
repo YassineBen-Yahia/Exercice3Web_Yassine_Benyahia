c0ab7cdec5a8a69e93f1caa195e8fc116133be86
yassine <yassinebnyahia@gmail.com>
2025-02-15 12:17:22 +0100

    exercice3

---
+++ todo.js
@@ -0,0 +1,42 @@
+
+/*enabling the to do button when all inputs arent empty*/
+function enbablingAddButtun(){
+    document.querySelectorAll(".input").forEach(input => {
+        input.addEventListener('input', () => {
+            const allFilled = [...document.querySelectorAll(".input")].every(input => input.value.trim() !== "");
+            document.querySelector(".btn").disabled = !allFilled;
+        });
+    });
+
+}
+enbablingAddButtun();
+
+
+/*ajoter les taches chaque fois le boutton est clické*/
+let html="";
+const AddButton =document.querySelector(".btn");
+AddButton.addEventListener('click',()=>{
+    const name=document.querySelector(".input_name").value;
+    const content=document.querySelector(".input_content").value;
+    const UniqueID=`${name}${content}`;
+    currentTask=`<div class="row deletable" >
+                <span class="task">${name}: ${content}
+                    <i class="bi bi-trash" ></i>
+                </span>
+                </div>`;
+    html+=currentTask;
+    document.querySelector(".Tasks").innerHTML=html;
+    document.querySelectorAll(".input").forEach(input=>input.value="");
+    document.querySelector(".btn").disabled =true;
+    
+});
+
+
+document.querySelector(".Tasks").addEventListener('click',function(e){
+    let rowToRemove=e.target.closest('.deletable');
+    rowToRemove.remove();
+    html= html.split(rowToRemove.innerHTML).join("");
+});
+
+
+
